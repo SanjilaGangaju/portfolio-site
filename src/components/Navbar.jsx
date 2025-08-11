2545d601880858c66968422d545f7463e46be0db
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-11 16:45:02 +0545

    decorate homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { LiaToggleOffSolid } from "react-icons/lia";
 const Navbar = () => {
   return (
     <>
-    <div className='flex items-center justify-between px-9 py-4'>
-         <nav className='font-saint grow text-redwood text-[2rem] font-semibold flex gap-10 items-center justify-center'>
-        <Link to="/">home</Link>
-        <Link to="/about">about me</Link>
-        <Link to="/projects">projects</Link>
+    <div className='flex items-center justify-start gap-10 px-9 pt-5'>
+        <nav className='font-saint  text-redwood text-[2rem] font-semibold flex gap-10 items-center justify-start'>
+        <Link to="/" className='relative after:absolute after:h-[2px] after:bottom-0 after:left-0 after:w-0 hover:after:w-full after:transition-all after:duration-300 after:bg-redwood'>home</Link>
+        <Link to="/about" className='relative after:absolute after:h-[2px] after:bottom-0 after:left-0 after:w-0 hover:after:w-full after:transition-all after:duration-300 after:bg-redwood'>about me</Link>
+        <Link to="/projects" className='relative after:absolute after:h-[2px] after:bottom-0 after:left-0 after:w-0 hover:after:w-full after:transition-all after:duration-300 after:bg-redwood'>projects</Link>
     </nav>
     <LiaToggleOffSolid className='text-[1.48rem] text-amber-800'></LiaToggleOffSolid>
     </div>
