c4afc582aea4944ac51ded36e3dffe62139865cf
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-12 19:44:40 +0545

    project section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,21 +2,35 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className='text-center flex flex-col items-center justify-center'>
-        <h2 className='font-imperial text-redwood text-4xl font-semibold'>My projects</h2>
+    <div className='text-center flex flex-col items-center justify-center p-8'>
+        <h2 className='font-imperial text-redwood text-4xl font-semibold mb-8'>My projects</h2>
 
-       <div className='grid grid-cols-2'>
-        <div>
-          Project -1
+       <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+        <div className='grid grid-cols-2 items-center justify-center gap-2'>
+          <p>AiResvue-Ai powered Resume Reviewer</p>
+           <div className=' border border-redwood shadow-lg rounded'>              
+                <img src='./src/assets/react-2.svg' className='w-full'></img>
+
+          </div>
+        </div>
+          <div className='grid grid-cols-2  items-center justify-center gap-2'>
+            <p>NomYmm-Food Recipe App with Api Integration</p>
+             <div className='border border-redwood shadow-lg rounded'>
+              
+                <img src='./src/assets/react-2.svg' className='w-full'></img>
+
+          </div>
         </div>
           <div>
-          Project -1
+          <p></p>
         </div>
-          <div>
-          Project -1
-        </div>
-          <div>
-          Project -1
+          <div className='grid grid-cols-2 items-center justify-center gap-2'>
+          <p>Oshune - Skincare Ecommerce Website wth Js</p>
+             <div className='border border-redwood shadow-lg rounded'>
+              
+                <img src='./src/assets/react-2.svg' className='w-full'></img>
+
+          </div>
         </div>
        </div>
       </div>
