46b02d50115451ffaaeab6739db420f7cfaa30b0
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-14 09:52:40 +0545

    add dark mode on off toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Projects from "./pages/Projects"
 const App = () => {
+  const [darkMode, setdarkMode] = useState(false);
   return (
     <>
     
     <Router>
-      <Navbar></Navbar>
+      <Navbar darkMode={darkMode} setdarkMode={setdarkMode}></Navbar>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/projects' element={<Projects></Projects>}></Route>
+        <Route path='/' element={<Home darkMode={darkMode}></Home>}></Route>
+        <Route path='/about' element={<About darkMode={darkMode}></About>}></Route>
+        <Route path='/projects' element={<Projects darkMode={darkMode}></Projects>}></Route>
       </Routes>
     </Router>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router'
 import { LiaToggleOffSolid } from "react-icons/lia";
+import { LiaToggleOnSolid } from 'react-icons/lia';
 
-const Navbar = () => {
+const Navbar = ({darkMode, setdarkMode}) => {
   return (
-    <>
+    <div className={darkMode?"darkmode":"lightmode"}>
     <div className='flex items-center justify-center gap-10 px-9 pt-5'>
-        <nav className='font-saint  text-redwood text-[2rem] font-semibold flex gap-10 items-center justify-start'>
+        <nav className='font-saint  text-[2rem] font-semibold flex gap-10 items-center justify-start'>
         <Link to="/" className='relative after:absolute after:h-[2px] after:bottom-0 after:left-0 after:w-0 hover:after:w-full after:transition-all after:duration-300 after:bg-redwood'>home</Link>
         <Link to="/about" className='relative after:absolute after:h-[2px] after:bottom-0 after:left-0 after:w-0 hover:after:w-full after:transition-all after:duration-300 after:bg-redwood'>about me</Link>
         <Link to="/projects" className='relative after:absolute after:h-[2px] after:bottom-0 after:left-0 after:w-0 hover:after:w-full after:transition-all after:duration-300 after:bg-redwood'>projects</Link>
     </nav>
-    <LiaToggleOffSolid className='text-[1.48rem] text-amber-800'></LiaToggleOffSolid>
+    <button onClick={()=>setdarkMode(!darkMode)}>  
+      {darkMode?(<LiaToggleOnSolid className='text-[1.48rem] text-amber-800'></LiaToggleOnSolid>):
+       (<LiaToggleOffSolid className='text-[1.48rem] text-amber-800'></LiaToggleOffSolid>)
+
+      } 
+    </button>
     </div>
    
-    </>
+    </div>
   )
 }
 
